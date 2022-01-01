126009fcb874005627824de344fd269134dd110f
HeikkiLu <heikki.luukkonen@gmail.com>
2022-01-02 01:38:38 +0200

    barebones ui

--- scripts/rockpaperscissors.js
+++ scripts/rockpaperscissors.js
@@ -1,70 +1,72 @@
-const options = ['rock', 'paper', 'scissors']
+const options = ["rock", "paper", "scissors"];
+let playerScore = 0;
+let computerScore = 0;
 
 function computerPlay() {
-	return options[Math.floor(Math.random() * options.length)]
+  return options[Math.floor(Math.random() * options.length)];
+}
+
+function play() {
+  const buttons = document.querySelectorAll("button");
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      game(button.id);
+    });
+  });
+}
+
+function setScore() {
+  const scoreboard = document.querySelector(".scoreboard");
+  scoreboard.textContent = `Player score: ${playerScore.toString()} Computer score: ${computerScore.toString()}`;
 }
 
 function playRound(playerSelection, computerSelection) {
-	playerSelection = playerSelection.toLowerCase()
-	computerSelection = computerSelection.toLowerCase()
+  playerSelection = playerSelection.toLowerCase();
+  computerSelection = computerSelection.toLowerCase();
 
-	console.log(`Player: ${playerSelection}\nComputer: ${computerSelection}`)
+  //console.log(`Player: ${playerSelection}\nComputer: ${computerSelection}`);
 
+  if (playerSelection === computerSelection) {
+    return 3;
+  }
 
-	if(playerSelection === computerSelection) {
-		return 3
-	}
+  if (playerSelection == "rock") {
+    if (computerSelection == "paper") {
+      return 0;
+    } else {
+      return 1;
+    }
+  }
 
-	if(playerSelection == 'rock') {
-		if(computerSelection == 'paper') {
-			return 0
-		} else {
-			return 1
-		}
-	}
+  if (playerSelection == "paper") {
+    if (computerSelection == "scissors") {
+      return 0;
+    } else {
+      return 1;
+    }
+  }
 
-	if(playerSelection == 'paper') {
-		if(computerSelection == 'scissors') {
-			return 0
-		} else {
-			return 1
-		}
-	}
-
-	if(playerSelection == 'scissors') {
-		if(computerSelection == 'paper') {
-			return 0
-		} else {
-			return 1
-		}
-	}
+  if (playerSelection == "scissors") {
+    if (computerSelection == "paper") {
+      return 0;
+    } else {
+      return 1;
+    }
+  }
 }
 
-function game() {
+function game(playerSelection) {
+  const computerSelection = computerPlay();
 
-	let playerScore = 0
-	let computerScore = 0
-
-	for(let i = 0; i < 5; i++) {
-		//const playerSelection = computerPlay()
-		const playerSelection = window.prompt("Rock, paper or scissors?")
-		const computerSelection = computerPlay()
-		let result = playRound(playerSelection, computerSelection)
-		if(result === 1) {
-			playerScore++
-		} else if(result === 0) {
-			computerScore++
-		} 
-	}
-
-	if(playerScore >= computerScore) {
-		return `Player wins! Result: ${playerScore} - ${computerScore}`
-	} else {
-		return `Computer wins! Result: ${computerScore} - ${playerScore}`
-	}
+  if (playerSelection != null) {
+    let result = playRound(playerSelection, computerSelection);
+    if (result === 1) {
+      playerScore++;
+    } else if (result === 0) {
+      computerScore++;
+    }
+  }
+  setScore();
 }
 
-
-
-console.log(game())
-
+play();
